70cabdd57e23e236e0a4d498cbb2f9daba24ccb6
cwkwebdesign <cwk.webdesign@gmail.com>
2015-11-15 11:51:59 -0600

    Made remove button work \!

--- script.js
+++ script.js
@@ -5,22 +5,24 @@ $(function() {
 		console.log(submission);
 
 		var $ul = $('<ul>');
-		$ul.text('Employee Review');
+		$ul.text(submission[0].value + '\'s Review');
 		$('section').append($ul);
 
 		for (var i = 0; i < submission.length; i++) {
 			var elem = submission[i].value;
+			var elem2 = submission[i].name;
 			var $li = $('<li>');
-			$li.text(elem);
+			$li.text(elem2 + ": " + elem);
 			$('ul:last').append($li);
 		}
 		$button = $('<button>');
 		$button.text('Remove');
 		$('ul:last').append($button);
-	})
+	});
 
-	// $('section').on('click', 'button' function(e) {
-	// 	event.preventDefault();
-	// 	$(this).closest("ul").detach();
-	// };
+
+//Why is this not working?!?! 
+	$('section').on('click', 'button', function() {
+		$(this).closest('ul').remove();
+	});
 });
