d3e9e55f33bb7ddd182308dd08e46c9191ef1e6b
cwkwebdesign <cwk.webdesign@gmail.com>
2015-11-15 19:55:58 -0600

    jQuery and style updates, basic challenge complete \!

--- script.js
+++ script.js
@@ -13,15 +13,38 @@ $(function() {
 			var elem2 = submission[i].name;
 			var $li = $('<li>');
 			$li.text(elem2 + ": " + elem);
-			$('ul:last').append($li);
-		}
+			if (elem2 === 'Review Score') {
+				switch (elem) {
+					case '0':
+						$li.addClass("rating0");
+						break;
+					case '1':
+						$li.addClass("rating1");
+						break;
+					case '2':
+						$li.addClass("rating2");
+						break;
+					case '3':
+						$li.addClass("rating3");
+						break;
+					case '4':
+						$li.addClass("rating4");
+						break;
+					case '5':
+						$li.addClass("rating5");
+						break;
+					default:
+						alert("Please enter a number between 0 and 5");
+				}
+			};
+			$('ul:last').append($li);			
+		};
+
 		$button = $('<button>');
 		$button.text('Remove');
 		$('ul:last').append($button);
 	});
 
-
-//Why is this not working?!?! 
 	$('section').on('click', 'button', function() {
 		$(this).closest('ul').remove();
 	});
